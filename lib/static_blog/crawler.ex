56d3f92992ed231dd98a458370a1077d61e6ab3b
Sebastian Seilund <sebsei@gmail.com>
2016-05-05 20:55:30 +0200

    Added Crawler

---
+++ lib/static_blog/crawler.ex
@@ -0,0 +1,11 @@
+defmodule StaticBlog.Crawler do
+  def crawl do
+    File.ls!("priv/posts")
+    |> Enum.map(&StaticBlog.Post.compile/1)
+    |> Enum.sort(&sort/2)
+  end
+
+  def sort(a, b) do
+    Timex.compare(a.date, b.date) > 0
+  end
+end
